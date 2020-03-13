a738ba9f18bfac735b6951162428cdffa8c4b377
Olaegbe Samuel <25031528+goodhands@users.noreply.github.com>
2020-03-13 23:43:00 +0100

    added vue form properites

--- src/components/NewTodo.vue
+++ src/components/NewTodo.vue
@@ -6,32 +6,32 @@
                 <h2 class="title">Create a new todo</h2>
                 <div class="group">
                     <label class="title is-6">Title</label>
-                    <input type="text" placeholder="Title" id="" class="input">
+                    <input type="text" placeholder="Title" v-model="todoTitle" class="input">
                 </div>
 
                 <a class="is-pulled-right" style="display: block" @click="showTask">+ Add new task</a>
 
                 <div class="create-tasks">                        
-                    <div class="task" v-if="showTaskForm" action="">
+                    <div class="task" v-if="showTaskForm">
                         <p class="subtitle is-6">Add a task to this todo</p>
                         <div class="field">
                             <label class="title is-6" for="title">Title</label>
                             <div class="control">   
-                                <input type="text" placeholder="Title" id="title" class="input">
+                                <input type="text" placeholder="Title"  v-model="taskTitle" id="title" class="input">
                             </div>
                         </div>
 
                         <div class="field">
                             <label class="title is-6" for="deadline">Deadline</label>
                             <div class="control">  
-                                <input type="date" placeholder="Deadline" id="deadline" class="input">
+                                <input type="date" placeholder="Deadline"  v-model="taskDeadline" id="deadline" class="input">
                             </div>
                         </div>
 
                         <div class="field">
                             <label class="title is-6" for="status">Status</label>
                             <div class="control">
-                                <input type="range" placeholder="Status" id="status">
+                                <input type="range" placeholder="Status" v-model="taskStatus" id="status">
                             </div>
                         </div>
 
@@ -53,19 +53,24 @@ export default {
             todoTitle: '',
             taskTitle: '',
             taskDeadline: '',
-            taskStatus: ''
+            taskStatus: '',
+            tasks: []
         }
     },
 
     methods: {
         saveTask(){
-
+            
         },
 
         showTask(){
             this.showTaskForm = !this.showTaskForm;
             return false;
         }
+    },
+
+    mounted() {
+        todoTitle.focus();
     },
 }
 </script>
